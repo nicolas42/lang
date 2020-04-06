a43056dc5cbf48bd51207491fcf7a206b86011b8
Nicolas Schmidt <nicolas42@gmail.com>
2020-04-06 15:07:08 +1000

    comment

--- path.sh
+++ path.sh
@@ -1,5 +1,6 @@
 cat $0
-# Please run this in the current shell with copy paste
+# Usage: source path.sh
+# The source command runs this command in the current shell so its effects are persistent - at least in mac os.
 PATH=$PATH:$(echo $(pwd)/bin)
 echo $PATH
 
