5748c8a1c6f07406cb2c3bfc3b95cd23c0d65148
Nicolas Schmidt <nicolas42@gmail.com>
2020-04-06 10:02:22 +1000

    starting

--- hello/hello.c
+++ hello/hello.c
@@ -1,3 +1,6 @@
+// push(){ git add . ; git commit -m $1 ; git push; }
+// clang -S -emit-llvm hello.c
+
 #include <stdio.h>
 
 // int factorial(int a){
@@ -9,6 +12,9 @@ int main(void){
 
 	printf("Hello World!");
 	printf("You say hello and I say goodbye!");
+
+    // int a,b,c;
+    // c = a + b;
 
 	// int a = factorial(3);
 	// printf("factorial(3) >> %d\n", a);
