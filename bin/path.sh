cbf0408159380521824b85e7e4363613548a1446
Nicolas Schmidt <nicolas42@gmail.com>
2020-04-06 15:10:13 +1000

    test

--- bin/path.sh
+++ bin/path.sh
@@ -1,6 +1,6 @@
 cat $0
 # Usage: source path.sh
 # The source command runs this command in the current shell so its effects are persistent - at least in mac os.
-PATH=$PATH:$(echo $(pwd)/bin)
+PATH=$PATH:$(echo $(pwd))
 echo $PATH
 
